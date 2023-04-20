3cdfc82db8729ca80e66039109e014661be6c65d
sandeep-1990 <46346470+sandeep-1990@users.noreply.github.com>
2023-04-20 09:02:37 +0530

    Add files via upload

---
+++ micoservices/src/main/java/com/learning/micoservices/MicoservicesApplication.java
@@ -0,0 +1,13 @@
+package com.learning.micoservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicoservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicoservicesApplication.class, args);
+	}
+
+}
---
+++ micoservices/src/test/java/com/learning/micoservices/MicoservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.micoservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicoservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
